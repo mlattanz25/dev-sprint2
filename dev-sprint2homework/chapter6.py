c15af54bdf06974438c96a3ebf01d0b7c5d6fafc
Michael Lattanzio <mlattanz25@Michaels-MacBook-Pro-2.local>
2013-08-25 19:52:37 -0400

    My Homework

---
+++ dev-sprint2homework/chapter6.py
@@ -0,0 +1,40 @@
+#6.6 Palindrome: A palindrome is a word spelled the same backwords and forward, like noon. 
+
+def first(word):
+	return word[0]
+def last (word):
+	return word [-1]
+def middle (word):
+	return word[1:-1]
+
+print middle ("Fo")
+
+#for some reason my function is not producing any answers for this part of the problem.
+
+def is_palindrome (string):
+	return is_palindrome.len
+	if is_palindrome.len = 9
+		return True
+	else
+		return False 
+
+is_palindrome (fox)
+
+
+#6.8 GCD
+
+def gcd (a, b):
+	gcd (a, 0)= a
+	if 
+	r = a/b 
+
+	gcd(a,b) = gcd(b, r)
+
+
+
+
+
+	
+
+
+
---
+++ dev-sprint2homework/chapter7.py
@@ -0,0 +1,12 @@
+# 7.5 Need symbols for the equation to properly fit this formula into the while loop. 
+	sum = 0 
+	i = 0 
+	while true:
+		math_function(k)
+			if x-sum < 1e-15
+			break
+			k + = 1
+			return k 
+
+		
+#Iterations
